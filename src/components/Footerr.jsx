be6ce3e591d359facac46961a7909d402054aa4f
Dollymm <100014305+Dollymm@users.noreply.github.com>
2023-08-10 21:17:38 +0530

    first commit

---
+++ src/components/Footerr.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footerr() {
+  return <footer>
+    <div>
+        <h1>TEchDolly</h1>
+        <p>@all right reseved</p>
+        </div>
+    <div>
+        <h5>Follw Us</h5>
+<div>
+    <a href='https://youtube.com/@dollymamgai7494' target='blank'>YouTube</a>
+    <a href='https://instagram.com/dolly_mamgai' target='blank'>instagram</a>
+    <a href='https://github.com/Dollymm' target='blank'>github</a>
+</div>
+
+    </div>
+
+  </footer>
+  
+};
+
+export default Footerr
